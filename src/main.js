810d9ae6da38692f8b2853f6ef01cc74edc02278
Max Poprawe <maximilian.poprawe@roche.com>
2020-11-20 16:26:31 +0100

    bugfix

--- src/main.js
+++ src/main.js
@@ -100,23 +100,16 @@ async function createUpdateOrDeleteAssociationLines(cbItem) {
 
 async function submitNewCodeBeamerItem(widget) {
   // get widget with all meta data (the selected one only has the general widget properties, but is lacking the type specifcs)
-  console.log(`widget before: ${JSON.stringify(widget)}`)
   widget = await getWidgetDetail({ id: widget.id })
-  console.log(`widget after: ${JSON.stringify(widget)}`)
   // generate submission object and submit
   let submissionItem = convert2CbItem(widget)
-  console.log(`submissionItem: ${JSON.stringify(submissionItem)}`)
   let cbItem = await addNewCbItem(submissionItem)
-  console.log(`cbItem before: ${JSON.stringify(cbItem)}`)
   await enrichBaseCbItemWithDetails(cbItem)
-  console.log(`cbItem after: ${JSON.stringify(cbItem)}`)
   // make card data from item
   let cardData = convert2Card(cbItem)
-  console.log(`cardData: ${JSON.stringify(cardData)}`)
   // set id to existing widget to overwrite it and update
   cardData.id = widget.id
   cbItem.Card = await createOrUpdateWidget(cardData)
-  console.log(`cbItem.Card : ${JSON.stringify(  cbItem.Card )}`)
 
   // no need to sync associations as the item was just created. Need to change if we add ability to link from miro
 }
@@ -335,7 +328,7 @@ function convert2CbItem(widget) {
 // ------------------------ Miro ------------------------------
 
 async function getWidgetDetail(widget) {
-  return await miro.board.widgets.get(widget)
+  return await miro.board.widgets.get(widget)[0]
 }
 
 async function findWidgetByTypeAndMetadataId(widgetData) {
