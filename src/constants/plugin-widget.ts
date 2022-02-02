307e76b53dc4a61d8981f48e090dbac52b32c4cf
URAN <urecha@emea.roche.com>
2022-02-02 14:54:44 +0100

    minor update plugin-widget.ts
    added a line to the description

--- src/constants/plugin-widget.ts
+++ src/constants/plugin-widget.ts
@@ -4,6 +4,7 @@ export const PLUGIN_WIDGET_TITLE =
 export const PLUGIN_WIDGET_DESCRIPTION =
 	"Don't delete me! <br> But if you do, I'll reappear when you reload the site.<br>" +
 	"codeBeamer-sync settings can be managed by clicking on the codeBeamer widget-toolbar icon, which appears when clicking this card.<br>" +
+	"<br><br>If you experience any problems, let us know at <a href='https://github.com/codeBeamer-Extensions-and-Addons/codebeamer-miro/issues'>our Github Repo</a>" +
 	"<br><br>[Infos from the Devs]<br> We're currently experiencing a bug with saving metadata on miro-widgets." +
 	"<br>This currently leads to the 'BoardSettings' (cb-Address, Project- & Inbox Tracker ID, user-mappings) to be saved on a per-user & per-board basis, instead of per-board." +
 	"<br>Mind that this could lead to duplicate ids and that synchronization of 'assignees' can therefore be asynchronous for different users.";
