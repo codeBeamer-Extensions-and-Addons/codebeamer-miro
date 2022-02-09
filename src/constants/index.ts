b9de35ba464d00e5d5ea74a7a1da18f51d1c9d0f
URAN <urecha@emea.roche.com>
2022-02-09 16:06:22 +0100

    add/refactor constants & constants barrel file
    - adds cb-item-name-field-name.ts
    - moves widget-initial-position-name.ts into constants folder

---
+++ src/constants/index.ts
@@ -0,0 +1,6 @@
+export * from './cb-import-defaults';
+export * from './cb-item-name-field-name';
+export * from './cb-relation-names';
+export * from './plugin-widget';
+export * from './svg';
+export * from './widget-initial-position-name';
