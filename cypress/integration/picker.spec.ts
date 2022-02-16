685c0f9a7c57067a0d5f980330ce11f251f0af7a
urecha <70019423+urecha@users.noreply.github.com>
2022-02-16 09:40:51 +0100

    Update picker.spec.ts
    - remove .only clause

--- cypress/integration/picker.spec.ts
+++ cypress/integration/picker.spec.ts
@@ -312,7 +312,7 @@ describe('Picker', () => {
     //refactor as needed when more tests come in
     context('dynamic elements without before-hook', () => {
         //* RETINA-1565415
-        it.only('does not display items of category Folder or Information in the results table', () => {    
+        it('does not display items of category Folder or Information in the results table', () => {    
             cy.intercept('GET', 'https://retinatest.roche.com/cb/api/v3/projects/**/trackers', { fixture: 'trackers.json' });        
             cy.intercept('POST', 'https://retinatest.roche.com/cb/api/v3/items/query', { fixture: 'trackerItems_with_categories' }).as('query');
             
