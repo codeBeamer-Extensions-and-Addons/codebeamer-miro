cf9c253754ad873fd17b03195971f573e8469536
urecha <70019423+urecha@users.noreply.github.com>
2022-02-16 11:50:14 +0100

    Update codebeamer.ts
    - updates/fixes getQueryEntityNameForCriteria() for non standard criteria

--- src/services/codebeamer.ts
+++ src/services/codebeamer.ts
@@ -367,14 +367,14 @@ export default class CodeBeamerService {
    * Maps FilterCriteria enum values to codeBeamer Query language entity names
    * @param criteria FilterCriteria as enum value or string (for custom fields)
    * @param trackerId Optional trackerId (required only for custom fields)
-   * @returns the matching codebeamer query language entity's name to a Filter Criteria, e.g. "teamName" for Team.
+   * @returns the matching codebeamer query language entity's name to a Filter Criteria, e.g. "teamName" for Team. For non-enumerated criteria, it constructs a customField query string.
    */
   public static getQueryEntityNameForCriteria(criteria: FilterCriteria | string, trackerId?: string): string {
     switch(criteria) {
       case FilterCriteria.TEAM: return 'teamName';
       case FilterCriteria.RELEASE: return 'release';
       case FilterCriteria.SUBJECT: return 'subjectName';
-      default: return `${trackerId ?? ''}.${criteria}`;
+      default: return `'${trackerId}.${criteria}'`;
     }
   }
   
