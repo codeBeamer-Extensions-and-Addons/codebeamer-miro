b7895290635a1eaa689032a38c2a425746926c00
urecha <70019423+urecha@users.noreply.github.com>
2022-02-25 15:17:56 +0100

    increase version nr to 0.12.0

--- src/init.ts
+++ src/init.ts
@@ -77,7 +77,7 @@ miro.onReady(async () => {
 	});
 
 	console.info(
-		`[codeBeamer-sync] Plugin v0.11.2 initialized. Experiencing issues? Let us know at https://github.com/codeBeamer-Extensions-and-Addons/codebeamer-miro/issues`
+		`[codeBeamer-sync] Plugin v0.12.0 initialized. Experiencing issues? Let us know at https://github.com/codeBeamer-Extensions-and-Addons/codebeamer-miro/issues`
 	);
 });
 
