4a9ec0917b250e93baf25a5fe120feb57813fb88
urecha <70019423+urecha@users.noreply.github.com>
2022-03-17 11:44:37 +0100

    increase version nr to 0.12.2
    And reformat package.json

--- src/init.ts
+++ src/init.ts
@@ -80,7 +80,7 @@ miro.onReady(async () => {
 	});
 
 	console.info(
-		`[codeBeamer-sync] Plugin v0.12.1 initialized. Experiencing issues? Let us know at https://github.com/codeBeamer-Extensions-and-Addons/codebeamer-miro/issues`
+		`[codeBeamer-sync] Plugin v0.12.2 initialized. Experiencing issues? Let us know at https://github.com/codeBeamer-Extensions-and-Addons/codebeamer-miro/issues`
 	);
 });
 
