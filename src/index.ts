e64145b3e2e9d8377125445b45c5e3573e7e1363
urecha <70019423+urecha@users.noreply.github.com>
2022-08-04 08:31:52 +0200

    update index.ts & app.tsx
    add some initial config & comment out boilerplate

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
 async function init() {
-  miro.board.ui.on('icon:click', async () => {
-    await miro.board.ui.openPanel({url: 'app.html'});
-  });
+	miro.board.ui.on('icon:click', async () => {
+		await miro.board.ui.openModal({
+			url: 'app.html',
+			height: 680,
+			width: 1080,
+		});
+	});
+
+	miro.board.ui.on('app_card:open', async (_event) => {
+		console.log('AppCardOpen Event: ', _event);
+	});
+
+	console.info(
+		`[codeBeamer-sync] Plugin v0.13.0 initialized. Experiencing issues? Let us know at https://github.com/codeBeamer-Extensions-and-Addons/codebeamer-miro/issues`
+	);
 }
 
 init();
