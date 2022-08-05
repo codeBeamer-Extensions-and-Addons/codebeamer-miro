c838ac995b32581805ce87f336b508998a396584
urecha <70019423+urecha@users.noreply.github.com>
2022-08-05 10:11:37 +0200

    add cypress & configure it
    configure cypress for component testing

---
+++ cypress.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  retries: {
+    runMode: 1,
+    openMode: 1,
+  },
+
+  projectId: "cumqrv",
+
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "vite",
+    },
+  },
+});
