11e10937889962af55a33c27e80308f26c277030
urecha <70019423+urecha@users.noreply.github.com>
2022-08-08 07:52:07 +0200

    rename apiConnectionSlice to userSettingsSlice
    allow for distinction to boardSettingsSlice and more accuracy in general

---
+++ src/store/userSettingsSlice.ts
@@ -0,0 +1,39 @@
+import { createSlice } from '@reduxjs/toolkit';
+import type { PayloadAction } from '@reduxjs/toolkit';
+import { UserSetting } from './userSetting.enum';
+
+export interface UserSettingsState {
+	cbAddress: string;
+	cbqlString: string;
+	cbUsername: string;
+	cbPassword: string;
+}
+
+const initialState: UserSettingsState = {
+	cbAddress: localStorage.getItem(UserSetting.CB_ADDRESS) ?? '',
+	cbqlString: localStorage.getItem(UserSetting.CBQL_STRING) ?? '',
+	cbUsername: localStorage.getItem(UserSetting.CB_USERNAME) ?? '',
+	cbPassword: sessionStorage.getItem(UserSetting.CB_PASSWORD) ?? '',
+};
+
+export const userSettingsSlice = createSlice({
+	name: 'userSettings',
+	initialState,
+	reducers: {
+		setCredentials: (
+			state,
+			action: PayloadAction<{ username: string; password: string }>
+		) => {
+			state.cbUsername = action.payload.username;
+			state.cbPassword = action.payload.password;
+
+			localStorage.setItem(UserSetting.CB_USERNAME, state.cbUsername);
+			sessionStorage.setItem(UserSetting.CB_PASSWORD, state.cbPassword);
+		},
+		//TODO etc.
+	},
+});
+
+export const { setCredentials } = userSettingsSlice.actions;
+
+export default userSettingsSlice.reducer;
