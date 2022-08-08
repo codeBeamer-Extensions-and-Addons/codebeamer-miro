71fc017c7d9c2fac89cc41fda76f8442ffc149d9
urecha <70019423+urecha@users.noreply.github.com>
2022-08-08 08:49:52 +0200

    update auth.cy.tsx
    add empty test cases

--- src/pages/auth/auth.cy.tsx
+++ src/pages/auth/auth.cy.tsx
@@ -1,11 +1,18 @@
 import Auth from './auth';
 
 describe('<Auth>', () => {
-	cy.stub(miro.board, 'getAppData').returns({
-		cbAddress: 'https://retina.roche.com',
-	});
-
 	it('mounts', () => {
 		cy.mount(<Auth loading={false} error={{}} />);
 	});
+
+	it.skip('loads cached values into the form', () => {
+		cy.stub(miro.board, 'getAppData').returns({
+			cbAddress: 'https://retina.roche.com',
+		});
+		//TODO fill/stub localstorage
+	});
+
+	it('validates the cb address before attempting auth');
+	it('does not attempt auth when values are missing');
+	it('saves the credentials when clicking the "Connect" button');
 });
