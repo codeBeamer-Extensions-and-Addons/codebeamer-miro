923a8f6bb6006ccecaf6e50fa5323450732bd534
urecha <70019423+urecha@users.noreply.github.com>
2022-08-08 07:50:41 +0200

    add board-/user setting enum
    split setting enum

---
+++ src/store/boardSetting.enum.ts
@@ -0,0 +1,7 @@
+export enum BoardSetting {
+	PROJECT_ID = 'projectId',
+	CB_ADDRESS = 'cbAddress',
+	INBOX_TRACKER_ID = 'inboxTrackerId',
+	USER_MAPPING = 'USER_MAPPING',
+	IMPORT_CONFIGURATION = 'importConfiguration',
+}
