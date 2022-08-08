a442c3192ac92df176769287601f3535ab53a59f
urecha <70019423+urecha@users.noreply.github.com>
2022-08-08 08:28:42 +0200

    update auth.cy.tsx
    add stub for miro.board.getAppData

--- src/pages/auth/auth.cy.tsx
+++ src/pages/auth/auth.cy.tsx
@@ -1,6 +1,10 @@
 import Auth from './auth';
 
 describe('<Auth>', () => {
+	cy.stub(miro.board, 'getAppData').returns({
+		cbAddress: 'https://retina.roche.com',
+	});
+
 	it('mounts', () => {
 		cy.mount(<Auth loading={false} error={{}} />);
 	});
