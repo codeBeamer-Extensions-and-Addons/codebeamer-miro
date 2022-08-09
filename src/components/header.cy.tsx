b4d0176bb9cf7b1bbe987061d1d16618de6a6cfe
urecha <70019423+urecha@users.noreply.github.com>
2022-08-09 08:59:24 +0200

    add tests for Header component
    not yet run due to cypress issue

---
+++ src/components/header.cy.tsx
@@ -0,0 +1,25 @@
+import Header from './header';
+
+describe('<Header>', () => {
+	it('mounts', () => {
+		cy.mount(<Header />);
+	});
+
+	it('can be centered by passing a prop', () => {
+		cy.mount(<Header centered={true} />);
+
+		cy.get('header')
+			.should('have.class', 'text-center')
+			.and('have.css', 'text-align', 'center');
+	});
+
+	it('displays passed children inside a <h1/> element', () => {
+		cy.mount(
+			<Header>
+				<p>test</p>
+			</Header>
+		);
+
+		cy.getBySel('header-container').find('p').should('have.text', 'test');
+	});
+});
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,9 @@ import * as React from 'react';
 export default function Header(props: { centered?: boolean; children?: any }) {
 	return (
 		<header className={props.centered ? 'text-center' : ''}>
-			<h1>{props.children}</h1>
+			<h1 className="h1" data-test="header-container">
+				{props.children}
+			</h1>
 		</header>
 	);
 }
