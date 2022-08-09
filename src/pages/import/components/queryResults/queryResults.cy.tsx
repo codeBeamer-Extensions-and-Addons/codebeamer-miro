938d1ad78ce03ecde2563348c5fec537cb056375
urecha <70019423+urecha@users.noreply.github.com>
2022-08-09 12:24:41 +0200

    Update queryResults.cy.tsx
    add tests
    not yet run due to cypress isues

--- src/pages/import/components/queryResults/queryResults.cy.tsx
+++ src/pages/import/components/queryResults/queryResults.cy.tsx
@@ -1,7 +1,32 @@
+import { setCbAddress } from '../../../../store/slices/boardSettingsSlice';
+import { setTrackerId } from '../../../../store/slices/userSettingsSlice';
+import { getStore } from '../../../../store/store';
 import QueryResults from './QueryResults';
 
 describe('<QueryResults>', () => {
 	it('mounts', () => {
 		cy.mount(<QueryResults />);
 	});
+
+	it('queries items with the cached cbqlString when mounted', () => {
+		const store = getStore();
+		const cbAddress = 'https://fake.codebeamer.com/cb';
+		const trackerId = '123';
+		const queryString = `tracker.id in (${trackerId})`;
+		//the cbqlString value in the store is set indirectly whenever value that it depends on change
+		store.dispatch(setTrackerId(trackerId));
+		store.dispatch(setCbAddress(cbAddress));
+
+		cy.intercept('POST', `${cbAddress}/items/query`, {
+			fixture: 'query.json',
+		}).as('itemQuery');
+
+		cy.mountWithStore(<QueryResults />, store);
+
+		cy.wait('@itemQuery')
+			.its('request.body.queryString')
+			.should('equal', queryString);
+	});
+
+	it('shows an info message when the tracker has no items');
 });
