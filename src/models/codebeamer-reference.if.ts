72bf1f0f8b371ecfc9d6458ef1f4920b03d1e856
urecha <70019423+urecha@users.noreply.github.com>
2022-08-11 13:52:32 +0200

    Update codebeamer-reference.if.ts
    add optional color property

--- src/models/codebeamer-reference.if.ts
+++ src/models/codebeamer-reference.if.ts
@@ -2,7 +2,9 @@
  * Generic reference an item can have as value for one of its properties
  */
 export interface CodeBeamerReference {
-    id: number;
-    name: string;
-    type: string;
+	id: number;
+	name: string;
+	keyName?: string;
+	color?: string;
+	type: string;
 }
