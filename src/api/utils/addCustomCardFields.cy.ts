7ebc1761de18b3e50d37d8519c82019ddc39a149
urecha <70019423+urecha@users.noreply.github.com>
2022-08-11 13:51:21 +0200

    add addCustomCardField function
    not fully tested since it took too long and isn't that pressing a matter

---
+++ src/api/utils/addCustomCardFields.cy.ts
@@ -0,0 +1,131 @@
+import { AppCard } from '@mirohq/websdk-types';
+import { CodeBeamerItem } from '../../models/codebeamer-item.if';
+import { store } from '../../store/store';
+import addCardFields from './addCustomCardFields';
+
+describe('addCardFields', () => {
+	it('adds a field for the Item its status if it has one', () => {
+		const expectedFieldValue = `Status: ${itemData.status.name}`;
+
+		const cardData = addCardFields({ id: '1' }, itemData);
+		expect(cardData.fields?.some((f) => f.value == expectedFieldValue)).to
+			.be.true;
+	});
+
+	context(
+		'when a property is set to true in the import configuration',
+		() => {
+			it('adds a field for this property to the card its data', () => {
+				cy.stub(store, 'getState').returns({
+					boardSettings: {
+						importConfiguration: {
+							standard: {
+								ID: true,
+							},
+						},
+					},
+				});
+
+				const expectedFieldValue = `ID: ${itemData.id}`;
+
+				const cardData = addCardFields({ id: '1' }, itemData);
+				expect(
+					cardData.fields?.some((f) => f.value == expectedFieldValue)
+				).to.be.true;
+			});
+
+			//TODO not that important
+			it.skip('adds a customField for the object-property that shows the object its name property', () => {
+				cy.stub(store, 'getState').returns({
+					boardSettings: {
+						importConfiguration: {
+							standard: {
+								modifiedBy: true,
+							},
+						},
+					},
+				});
+
+				const expectedFieldValue = `Modified By: ${itemData.modifiedBy.name}`;
+
+				const cardData = addCardFields({ id: '1' }, itemData);
+				console.log(cardData.fields);
+				expect(
+					cardData.fields?.some((f) => f.value == expectedFieldValue)
+				).to.be.true;
+			});
+
+			it.skip(
+				'adds a customField with the comma-seperated name properties of each entry of an array-property'
+			);
+		}
+	);
+});
+
+const itemData: CodeBeamerItem = {
+	id: 1,
+	name: 'Copy of superordinate',
+	description: 'I wanna be',
+	descriptionFormat: 'Wiki',
+	createdAt: '2022-02-11T07:34:40.313',
+	createdBy: {
+		id: 2,
+		name: 'tester',
+		type: 'UserReference',
+		email: 'tester@cypress.io',
+	},
+	modifiedAt: '2022-02-14T10:21:29.601',
+	modifiedBy: {
+		id: 2,
+		name: 'tester',
+		type: 'UserReference',
+		email: 'tester@cypress.io',
+	},
+	owners: [
+		{
+			id: 2,
+			name: 'tester',
+			type: 'UserReference',
+			email: 'tester@cypress.io',
+		},
+	],
+	version: 6,
+	assignedTo: [
+		{
+			id: 2,
+			name: 'tester',
+			type: 'UserReference',
+			email: 'tester@cypress.io',
+		},
+		{
+			id: 3,
+			name: 'lester',
+			type: 'UserReference',
+			email: 'lester@cypress.io',
+		},
+	],
+	tracker: {
+		id: 18,
+		name: 'Miro sync test',
+		type: 'TrackerReference',
+	},
+	children: [],
+	customFields: [],
+	priority: {
+		id: 0,
+		name: 'Unset',
+		type: 'ChoiceOptionReference',
+	},
+	status: {
+		id: 1,
+		name: 'New',
+		type: 'ChoiceOptionReference',
+	},
+	categories: [],
+	versions: [],
+	ordinal: 0,
+	typeName: 'Issue',
+	comments: [],
+	subjects: [],
+	teams: [],
+};
---
+++ src/api/utils/addCustomCardFields.ts
@@ -0,0 +1,84 @@
+import { AppCard } from '@mirohq/websdk-types';
+import { CB_ITEM_NAME_PROPERTY_NAME } from '../../constants/cb-item-name-field-name';
+import { StandardItemProperty } from '../../enums/standard-item-property.enum';
+import { CodeBeamerItem } from '../../models/codebeamer-item.if';
+import { ImportConfiguration } from '../../models/import-configuration.if';
+import { store } from '../../store/store';
+import getCodeBeamerPropertyNameByFieldLabel from './getCodeBeamerPropertyNameByFieldLabel';
+import getColorForFieldLabel from './getColorForFieldLabel';
+
+export default function addCardFields(
+	cardData: Partial<AppCard>,
+	item: CodeBeamerItem
+): Partial<AppCard> {
+	const NO_IMPORT_CONFIGURATION = 'No import configuration defined';
+	let importConfiguration = store.getState().boardSettings
+		.importConfiguration as ImportConfiguration;
+
+	if (!importConfiguration) throw new Error(NO_IMPORT_CONFIGURATION);
+
+	const standardConfiguration = importConfiguration.standard;
+	const standardConfigurationKeys = Object.keys(standardConfiguration);
+
+	if (!cardData.fields) cardData.fields = [];
+
+	//a foreach on Object.keys got me the ky's indexes instead of keys as entries.
+	for (let i = 0; i < standardConfigurationKeys.length; i++) {
+		const key = standardConfigurationKeys[i] as StandardItemProperty;
+		const value = standardConfiguration[key];
+
+		if (value == false) continue;
+		let field;
+
+		const itemPropertyName = getCodeBeamerPropertyNameByFieldLabel(key);
+		try {
+			field = item[itemPropertyName as keyof CodeBeamerItem];
+			if (!field) throw new Error("Field doesn't exist on Item");
+		} catch (error) {
+			continue;
+		}
+
+		let content: string;
+
+		if (field == null) continue;
+
+		if (typeof field === 'object') {
+			if (Array.isArray(field)) {
+				//* display comma-seperated names of all entries
+				content = '';
+				for (let j = 0; j < field.length; j++) {
+					let entry = field[j];
+					let slug = entry[CB_ITEM_NAME_PROPERTY_NAME];
+					content += `${slug}, `;
+				}
+				//remove trailing ", "
+				content = content.substring(0, content.length - 2);
+			} else {
+				//* display the name-property
+				content = field[CB_ITEM_NAME_PROPERTY_NAME];
+			}
+		} else {
+			//* just show the field
+			content = field.toString();
+		}
+
+		let customField = {
+			//TODO custom colors
+			fillColor: getColorForFieldLabel(key),
+			textColor: '#ffffff',
+			value: `${key}: ${content}`,
+		};
+		cardData.fields.push(customField);
+	}
+
+	//status is always displayed, if the item has one
+	if (item.status) {
+		cardData.fields.push({
+			fillColor: '#4f8ae8',
+			textColor: '#ffffff',
+			value: `Status: ${item.status.name}`,
+		});
+	}
+
+	return cardData;
+}
