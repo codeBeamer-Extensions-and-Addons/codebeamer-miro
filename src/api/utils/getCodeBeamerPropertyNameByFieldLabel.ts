fc1931271497da97eb2a5db6c108efecd5e41048
urecha <70019423+urecha@users.noreply.github.com>
2022-08-11 13:51:35 +0200

    Create getCodeBeamerPropertyNameByFieldLabel.ts

---
+++ src/api/utils/getCodeBeamerPropertyNameByFieldLabel.ts
@@ -0,0 +1,41 @@
+import { StandardItemProperty } from '../../enums/standard-item-property.enum';
+
+/**
+ * Gets the static codeBeamer Item property name for a {@link StandardItemProperty} value
+ * @param fieldLabel
+ * @returns Name of the codeBeamer item property labelled by {@link fieldLabel}
+ */
+export default function getCodeBeamerPropertyNameByFieldLabel(
+	fieldLabel: StandardItemProperty
+): string {
+	switch (fieldLabel) {
+		case StandardItemProperty.ID:
+			return 'id';
+		case StandardItemProperty.TEAMS:
+			return 'teams';
+		case StandardItemProperty.OWNER:
+			return 'owners';
+		case StandardItemProperty.RELEASE:
+			return 'release';
+		case StandardItemProperty.PRIORITY:
+			return 'namedPriority';
+		case StandardItemProperty.STORY_POINTS:
+			return 'storyPoints';
+		case StandardItemProperty.START_DATE:
+			return 'startDate';
+		case StandardItemProperty.END_DATE:
+			return 'endDate';
+		case StandardItemProperty.ASSIGNED_TO:
+			return 'assignedTo';
+		case StandardItemProperty.ASSIGNED_AT:
+			return 'assignedAt';
+		case StandardItemProperty.SUBMITTED_AT:
+			return 'submittedAt';
+		case StandardItemProperty.SUBMITTED_BY:
+			return 'submitter';
+		case StandardItemProperty.MODIFIED_AT:
+			return 'modifiedAt';
+		case StandardItemProperty.MODIFIED_BY:
+			return 'modifier';
+	}
+}
