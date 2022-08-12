debd49cabd43512c304980e7416e27e27ad4d168
urecha <70019423+urecha@users.noreply.github.com>
2022-08-12 10:31:48 +0200

    Update miro.api.ts
    revert to using AppCards

--- src/api/miro.api.ts
+++ src/api/miro.api.ts
@@ -1,12 +1,13 @@
-import { AppCard, Card } from '@mirohq/websdk-types';
+import { AppCard } from '@mirohq/websdk-types';
 import { CodeBeamerItem } from '../models/codebeamer-item.if';
 import addCardFields from './utils/addCardFields';
 import getCbItemUrl from './utils/getCbItemUrl';
 import getItemColorField from './utils/getItemColorField';
 import getRandomizedInitialCoordSetInViewport from './utils/getRandomizedInitialCoordSetInViewport';
 
+//TODO probably remove the "update" part
 export async function createOrUpdateItem(item: CodeBeamerItem) {
-	const card: Partial<Card> = await convertToCardData(item);
+	const card: Partial<AppCard> = await convertToCardData(item);
 	const coords = await getRandomizedInitialCoordSetInViewport();
 	card.x = coords.x;
 	card.y = coords.y;
@@ -19,27 +20,29 @@ export async function createOrUpdateItem(item: CodeBeamerItem) {
 			//*then it couldn't be found, so it usually doesn't exist
 		}
 	} else {
-		const widget = await miro.board.createCard({
+		const widget = await miro.board.createAppCard({
 			...card,
 		});
 	}
 }
 
-async function convertToCardData(item: CodeBeamerItem): Promise<Partial<Card>> {
-	let cardData: Partial<Card> = {
+export async function convertToCardData(
+	item: CodeBeamerItem
+): Promise<Partial<AppCard>> {
+	let cardData: Partial<AppCard> = {
 		// id: item.id.toString(),
 		title: `<a href="${getCbItemUrl(item.id.toString())}">${item.name} - [${
 			item.tracker.keyName + '-' ?? ''
 		}${item.id}]</a>`,
 		description: item.description,
-		// fields: [],
+		fields: [],
 	};
 
-	// try {
-	// 	addCardFields(cardData, item);
-	// } catch (err: any) {
-	// 	//TODO miro.showErrorNotif
-	// }
+	try {
+		addCardFields(cardData, item);
+	} catch (err: any) {
+		//TODO miro.showErrorNotif
+	}
 
 	// background Color
 	let colorFieldValue = getItemColorField(item);
