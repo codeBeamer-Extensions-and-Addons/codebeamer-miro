dd3c166658c320b74642b75dc2d9aa6ce4997215
urecha <70019423+urecha@users.noreply.github.com>
2022-08-25 08:35:48 +0200

    update AppCardTagSettings component
    add (presumably all) functionality, tests & styling

--- src/pages/import/components/settings/cardCustomization/AppCardTagSettings.tsx
+++ src/pages/import/components/settings/cardCustomization/AppCardTagSettings.tsx
@@ -1,8 +1,89 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { StandardItemProperty } from '../../../../../enums/standard-item-property.enum';
+import { setStandardCardTagConfiguration } from '../../../../../store/slices/boardSettingsSlice';
+import { RootState } from '../../../../../store/store';
+
+import './appCardTagSettings.css';
 
 export default function AppCardTagSettings() {
+	const dispatch = useDispatch();
+
+	const { cardTagConfiguration } = useSelector(
+		(state: RootState) => state.boardSettings
+	);
+
 	const standardProperties: string[] = Object.values(StandardItemProperty);
 
-	return <div>CardCustomizationSettings</div>;
+	return (
+		<div>
+			<div>
+				<p className="muted-medium">
+					Select the properties you want to be displayed as Tags on
+					the imported Items' Cards.
+				</p>
+			</div>
+			<div className="my-2">
+				<div className="property">
+					<label
+						className="checkbox"
+						title="Default property"
+						data-test="defaultTag-summary"
+					>
+						<input type="checkbox" disabled checked />
+						<span className="muted-medium">Summary</span>
+					</label>
+				</div>
+				<div className="property">
+					<label
+						className="checkbox"
+						data-test="defaultTag-description"
+					>
+						<input
+							type="checkbox"
+							title="Default property"
+							disabled
+							checked
+						/>
+						<span className="muted-medium">Description</span>
+					</label>
+				</div>
+				<div className="property">
+					<label
+						className="checkbox"
+						title="Default property"
+						data-test="defaultTag-status"
+					>
+						<input type="checkbox" disabled checked />
+						<span className="muted-medium">Status</span>
+					</label>
+				</div>
+				{standardProperties.map((p) => (
+					<div className="property">
+						<label
+							className="checkbox"
+							data-test={`tag-${p.replace(' ', '-')}`}
+						>
+							<input
+								type="checkbox"
+								checked={cardTagConfiguration.standard[p]}
+								onChange={() =>
+									dispatch(
+										setStandardCardTagConfiguration({
+											property: p,
+											value:
+												!cardTagConfiguration.standard[
+													p
+												] ?? true,
+										})
+									)
+								}
+							/>
+							<span>{p}</span>
+						</label>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 }
--- src/pages/import/components/settings/cardCustomization/appCardTagSettings.cy.tsx
+++ src/pages/import/components/settings/cardCustomization/appCardTagSettings.cy.tsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import { StandardItemProperty } from '../../../../../enums/standard-item-property.enum';
+import { IAppCardTagSetting } from '../../../../../models/import-configuration.if';
+import { setStandardCardTagConfiguration } from '../../../../../store/slices/boardSettingsSlice';
+import { getStore } from '../../../../../store/store';
 import AppCardTagSettings from './AppCardTagSettings';
 
 describe('<AppCardTagSettings', () => {
@@ -10,17 +14,105 @@ describe('<AppCardTagSettings', () => {
 		cy.mountWithStore(<AppCardTagSettings />);
 
 		cy.getBySel('defaultTag-summary')
-			.find('input[@type="checkbox"]')
+			.find('input[type="checkbox"]')
 			.should('be.disabled');
 		cy.getBySel('defaultTag-description')
-			.find('input[@type="checkbox"]')
+			.find('input[type="checkbox"]')
 			.should('be.disabled');
 		cy.getBySel('defaultTag-status')
-			.find('input[@type="checkbox"]')
+			.find('input[type="checkbox"]')
 			.should('be.disabled');
 	});
 
-	it('shows a checkbox for each value of the StandardItemProperty');
+	it('shows a checkbox for each value of the StandardItemProperty', () => {
+		const standardProperties: string[] =
+			Object.values(StandardItemProperty);
 
-	it('updates the stored cardTagConfiguration when a value is (un-)checked');
+		cy.mountWithStore(<AppCardTagSettings />);
+
+		standardProperties.forEach((p) => {
+			cy.getBySel(`tag-${p.replace(' ', '-')}`)
+				.find('input[type="checkbox"]')
+				.should('exist');
+		});
+	});
+
+	context('store / cache interface', () => {
+		it('sets the inputs their "checked" value based on the cached cardTagConfiguration', () => {
+			const testSettings: IAppCardTagSetting[] = [
+				{
+					property: 'ID',
+					value: true,
+				},
+				{
+					property: 'Story Points',
+					value: true,
+				},
+				{
+					property: 'Submitted By',
+					value: true,
+				},
+				{
+					property: 'Submitted At',
+					value: false,
+				},
+			];
+
+			const store = getStore();
+
+			for (let i = 0; i < testSettings.length; i++) {
+				store.dispatch(
+					setStandardCardTagConfiguration(testSettings[i])
+				);
+			}
+
+			cy.mountWithStore(<AppCardTagSettings />, { reduxStore: store });
+
+			for (let i = 0; i < testSettings.length; i++) {
+				cy.getBySel(`tag-${testSettings[i].property.replace(' ', '-')}`)
+					.find('input[type="checkbox"]')
+					.then((element) => {
+						if (testSettings[i].value) {
+							expect(element).to.be.checked;
+						} else {
+							expect(element).not.to.be.checked;
+						}
+					});
+			}
+		});
+
+		it.only('updates the stored cardTagConfiguration when a value is (un-)checked', () => {
+			const store = getStore();
+
+			cy.spy(store, 'dispatch').as('dispatch');
+
+			cy.mountWithStore(<AppCardTagSettings />, { reduxStore: store });
+
+			cy.getBySel('tag-Owner').click();
+			cy.get('@dispatch').then((spy) => {
+				expect(spy).to.have.been.calledWith(
+					setStandardCardTagConfiguration({
+						property: 'Owner',
+						value: true,
+					})
+				);
+			});
+
+			cy.wait(500);
+
+			cy.getBySel('tag-Owner').click();
+			cy.get('@dispatch').then((spy) => {
+				expect(spy).to.have.been.calledWith(
+					setStandardCardTagConfiguration({
+						property: 'Owner',
+						value: false,
+					})
+				);
+			});
+		});
+	});
+
+	afterEach(() => {
+		localStorage.clear();
+	});
 });
