81dfc8f80fa1d19fc28e820c15f2eb7b41f15787
urecha <70019423+urecha@users.noreply.github.com>
2022-08-26 12:32:38 +0200

    refactor activeFilters & filterCriteria test files
    move AND/OR functionality tests in correct file (activeFilters.cy.tsx)

--- src/pages/import/components/query/activeFilters/activeFilters.cy.tsx
+++ src/pages/import/components/query/activeFilters/activeFilters.cy.tsx
@@ -32,4 +32,24 @@ describe('<ActiveFilters>', () => {
 	it(
 		'removes a filter criteria from store when its component its remove-button is clicked'
 	);
+
+	//TODO If I got time. Else, submit a US.
+	describe.skip('AND/OR', () => {
+		it('has a button that allows showing a text input when it his hidden');
+		it('has a button to remove the text input when it is shown');
+
+		describe('Input', () => {
+			it('validates the input to only contain the valid characters'); //being AND, OR and brackets
+			it(
+				'validates the input to only contain numbers that have respective active filter criteria'
+			); //by their id(+1)
+
+			it('displays the input text in green when it is valid');
+			it('displays the input text in red when it is invalid');
+
+			it(
+				'updates the stored AND/OR string, with filter IDs replaced by their respective CBQL, when submitting'
+			);
+		});
+	});
 });
