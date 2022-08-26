c0578dcf89b2b275ffa5aa5edd152c03e5077648
urecha <70019423+urecha@users.noreply.github.com>
2022-08-26 12:24:33 +0200

    add FilterCriteria component
    add boilerplate component
    add requirements as test-cases

---
+++ src/pages/import/components/query/filterCriteria/FilterCriteria.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { IFilterCriteria } from '../../../../../models/filterCriteria.if';
+
+export default function FilterCriteria(props: {
+	filterCriteria: IFilterCriteria;
+	onRemove: Function;
+}) {
+	return <div>FilterCriteria</div>;
+}
