2cce248b4ac3b5ef3c0cf29391414420def5f74c
urecha <70019423+urecha@users.noreply.github.com>
2022-08-26 13:02:15 +0200

    implement FilterCriteria component

--- src/pages/import/components/query/filterCriteria/FilterCriteria.tsx
+++ src/pages/import/components/query/filterCriteria/FilterCriteria.tsx
@@ -1,9 +1,39 @@
 import React from 'react';
 import { IFilterCriteria } from '../../../../../models/filterCriteria.if';
 
+import './filterCriteria.css';
+
 export default function FilterCriteria(props: {
 	filterCriteria: IFilterCriteria;
 	onRemove: Function;
+	showId?: boolean;
 }) {
-	return <div>FilterCriteria</div>;
+	return (
+		<span className="tag" data-test={`criteria-${props.filterCriteria.id}`}>
+			{`${props.showId ? props.filterCriteria.id + ') ' : ''}${
+				props.filterCriteria.slug
+			}: ${props.filterCriteria.value}`}
+			<div
+				className="filter-remove"
+				data-test={`remove-${props.filterCriteria.id}`}
+				onClick={() => props.onRemove(props.filterCriteria.id)}
+			>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					className="ionicon"
+					viewBox="0 0 512 512"
+				>
+					<title>Remove Filter</title>
+					<path
+						fill="none"
+						stroke="currentColor"
+						strokeLinecap="round"
+						strokeLinejoin="round"
+						strokeWidth="32"
+						d="M368 368L144 144M368 144L144 368"
+					/>
+				</svg>
+			</div>
+		</span>
+	);
 }
--- src/pages/import/components/query/filterCriteria/filterCriteria.cy.tsx
+++ src/pages/import/components/query/filterCriteria/filterCriteria.cy.tsx
@@ -3,11 +3,14 @@ import { IFilterCriteria } from '../../../../../models/filterCriteria.if';
 import FilterCriteria from './FilterCriteria';
 
 const testCriteria: IFilterCriteria = {
-	id: 0,
+	id: 1,
 	slug: 'Language',
 	fieldName: 'Language',
 	value: 'FR',
 };
+
+const criteriaSelector = `criteria-${testCriteria.id}`;
+const removeButtonSelector = `remove-${testCriteria.id}`;
 
 describe('<FilterCriteria>', () => {
 	it('mounts', () => {
@@ -16,8 +19,58 @@ describe('<FilterCriteria>', () => {
 		);
 	});
 
-	it('displays given filter its slug');
-	it('displays given filter its value');
-	it('has a button to remove it with');
-	it('calls the passed handler when the remove-button is clicked');
+	context('with default data', () => {
+		beforeEach(() => {
+			cy.mountWithStore(
+				<FilterCriteria
+					filterCriteria={testCriteria}
+					onRemove={() => {}}
+				/>
+			);
+		});
+
+		it('displays given filter its slug', () => {
+			cy.getBySel(criteriaSelector).should(
+				'contain.text',
+				testCriteria.slug
+			);
+		});
+
+		it('displays given filter its value', () => {
+			cy.getBySel(criteriaSelector).should(
+				'contain.text',
+				testCriteria.value
+			);
+		});
+
+		it('has a button to remove it with', () => {});
+	});
+
+	it('displays given filter its ID when enabled in its call', () => {
+		cy.mountWithStore(
+			<FilterCriteria
+				filterCriteria={testCriteria}
+				onRemove={() => {}}
+				showId={true}
+			/>
+		);
+
+		cy.getBySel(criteriaSelector).should('contain.text', testCriteria.id);
+	});
+
+	it('calls the passed handler with the criteria its id as arg when the remove-button is clicked', () => {
+		const spy = cy.spy().as('spy');
+
+		cy.mountWithStore(
+			<FilterCriteria
+				filterCriteria={testCriteria}
+				onRemove={spy}
+				showId={true}
+			/>
+		);
+
+		cy.getBySel(removeButtonSelector).click();
+
+		cy.get('@spy').should('have.been.calledWith', testCriteria.id);
+	});
 });
