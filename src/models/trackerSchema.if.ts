ba66d187b62b6daa0f881a84f723627d14ae0603
urecha <70019423+urecha@users.noreply.github.com>
2022-08-29 08:08:59 +0200

    Create trackerSchema.if.ts

---
+++ src/models/trackerSchema.if.ts
@@ -0,0 +1,8 @@
+/**
+ * Notworthy properties of an entry in a Tracker's schema array
+ */
+export interface CodeBeamerTrackerSchemaEntry {
+	name: string;
+	legacyRestName: string;
+	trackerItemField: string;
+}
