3f35a187f5fa6f022592f9cae15a60ee022f78e3
urecha <70019423+urecha@users.noreply.github.com>
2022-08-29 10:52:54 +0200

    minor update dev reset logic app.tsx
    add another line to reset some values on emand

--- src/app.tsx
+++ src/app.tsx
@@ -7,10 +7,14 @@ import { store } from './store/store';
 import Content from './pages/content/Content';
 
 function App() {
-	//manual projectId reset because I haven't implemented it yet in the UI
+	//* manual projectId reset because I haven't implemented it yet in the UI
 	// React.useEffect(() => {
-	// 	miro.board.setAppData('projectId', '');
-	// 	miro.board.setAppData('cbAddress', '');
+	// miro.board.setAppData('projectId', '');
+	// miro.board.setAppData('cbAddress', '');
+	// miro.board.setAppData(
+	// 	'cardTagConfiguration',
+	// 	'{ standard: {}, trackerSpecific: {} }'
+	// );
 	// });
 
 	return (
