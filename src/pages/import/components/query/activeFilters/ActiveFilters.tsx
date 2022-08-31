8ec5a1e387f2709336f794ec8dc436e70e70e903
urecha <70019423+urecha@users.noreply.github.com>
2022-08-31 10:54:02 +0200

    update ActiveFilters component w/ AND/OR input
    add a codeBeamer-inspired AND/OR input
    lacks pattern validation and instead currently only validates via query success/failure

--- src/pages/import/components/query/activeFilters/ActiveFilters.tsx
+++ src/pages/import/components/query/activeFilters/ActiveFilters.tsx
@@ -1,6 +1,12 @@
+import { Field, Formik } from 'formik';
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { removeFilter } from '../../../../../store/slices/userSettingsSlice';
+import {
+	removeFilter,
+	resetCbqlStringToCurrentParameters,
+	setAndOrFilter,
+	setAndOrFilterEnabled,
+} from '../../../../../store/slices/userSettingsSlice';
 import { RootState } from '../../../../../store/store';
 import FilterCriteria from '../filterCriteria/FilterCriteria';
 
@@ -9,7 +15,7 @@ import './activeFilters.css';
 export default function ActiveFilters() {
 	const dispatch = useDispatch();
 
-	const { activeFilters } = useSelector(
+	const { activeFilters, andOrFilterEnabled, andOrFilter } = useSelector(
 		(state: RootState) => state.userSettings
 	);
 
@@ -17,18 +23,125 @@ export default function ActiveFilters() {
 		dispatch(removeFilter(id));
 	};
 
+	const removeAndOr = () => {
+		dispatch(setAndOrFilterEnabled(false));
+		dispatch(resetCbqlStringToCurrentParameters());
+	};
+
 	return (
-		<div className="activeFilters">
-			{activeFilters.map((f, i) => (
-				<FilterCriteria
-					key={
-						`${f.slug}-${f.value}-${f.id}` /*NOT the id, because it will be reused */
-					}
-					showId={false}
-					filterCriteria={f}
-					onRemove={removeCriteria}
-				/>
-			))}
+		<div className="grid activeFiltersWrapper">
+			<div
+				className={`activeFilters cs1 ${
+					andOrFilterEnabled ? 'ce8' : 'ce9'
+				}`}
+			>
+				{activeFilters.map((f, i) => (
+					<FilterCriteria
+						key={
+							`${f.slug}-${f.value}-${f.id}` /*NOT just the id, because it will be reused */
+						}
+						showId={andOrFilterEnabled}
+						filterCriteria={f}
+						onRemove={removeCriteria}
+					/>
+				))}
+			</div>
+			<div className={`${andOrFilterEnabled ? 'cs9' : 'cs10'} ce12`}>
+				{!andOrFilterEnabled && (
+					<span
+						className="link-label"
+						title="Add AND/OR logic"
+						onClick={() => {
+							dispatch(setAndOrFilterEnabled(true));
+							dispatch(setAndOrFilter(andOrFilter));
+						}}
+						data-test="showAndOr"
+					>
+						<span className="link-label-icon">+</span>{' '}
+						<span className="link-label-text">AND/OR</span>
+					</span>
+				)}
+				{andOrFilterEnabled && (
+					<div>
+						<Formik
+							initialValues={{ andOrLogic: andOrFilter ?? '' }}
+							enableReinitialize={true}
+							//* leaving this failed attempt here, to potentially accelerate revamp attempts
+							// validate={(values) => {
+							// 	const errors: { andOrLogic?: string } = {};
+
+							// 	const allowedIds = activeFilters
+							// 		.map((f) => (f.id! + 1).toString())
+							// 		.join('');
+
+							// 	//* not quite what we'd need
+							// 	const regex: RegExp = new RegExp(
+							// 		`^(\\(?)+|(([${allowedIds}]{1})? (AND|OR) \\(?[${allowedIds}]{1})+(\\)?)+`,
+							// 		'g'
+							// 	);
+
+							// 	console.log(regex.source);
+
+							// 	console.log(
+							// 		'Validated: ',
+							// 		regex.test(values.andOrLogic)
+							// 	);
+
+							// 	if (!regex.test(values.andOrLogic))
+							// 		errors.andOrLogic = 'Invalid AND/OR Logic';
+
+							// 	return errors;
+							// }}
+							onSubmit={(values) => {
+								dispatch(setAndOrFilter(values.andOrLogic));
+							}}
+							validateOnChange={true}
+						>
+							{({ errors, handleSubmit }) => (
+								<form onSubmit={handleSubmit}>
+									<div className="grid">
+										<div className="cs1 ce10">
+											<Field
+												name="andOrLogic"
+												placeholder="e.g. (1 OR 2)"
+												className={`input input-small ${
+													errors.andOrLogic
+														? 'error'
+														: 'success'
+												}`}
+												data-test="andOrInput"
+											></Field>
+										</div>
+										<div className="cs11 ce12">
+											<div
+												className="filter-remove v-align-middle bg-light-indigo"
+												onClick={() => removeAndOr()}
+												data-test="hideAndOr"
+											>
+												<svg
+													xmlns="http://www.w3.org/2000/svg"
+													className="ionicon v-align-sub"
+													viewBox="0 0 512 512"
+												>
+													<title>Cancel</title>
+													<path
+														fill="none"
+														stroke="currentColor"
+														strokeLinecap="round"
+														strokeLinejoin="round"
+														strokeWidth="32"
+														d="M368 368L144 144M368 144L144 368"
+													/>
+												</svg>
+											</div>
+										</div>
+									</div>
+								</form>
+							)}
+						</Formik>
+					</div>
+				)}
+			</div>
 		</div>
 	);
 }
--- src/pages/import/components/query/activeFilters/activeFilters.cy.tsx
+++ src/pages/import/components/query/activeFilters/activeFilters.cy.tsx
@@ -3,6 +3,9 @@ import { IFilterCriteria } from '../../../../../models/filterCriteria.if';
 import {
 	addFilter,
 	removeFilter,
+	resetCbqlStringToCurrentParameters,
+	setAndOrFilter,
+	setAndOrFilterEnabled,
 } from '../../../../../store/slices/userSettingsSlice';
 import { getStore } from '../../../../../store/store';
 import ActiveFilters from './ActiveFilters';
@@ -27,6 +30,10 @@ const testCriteria: IFilterCriteria[] = [
 		value: 'Aurelieng',
 	},
 ];
+
+const showAndOrButtonSelector = 'showAndOr';
+const hideAndOrButtonSelector = 'hideAndOr';
+const andOrInputSelector = 'andOrInput';
 
 describe('<ActiveFilters>', () => {
 	it('mounts', () => {
@@ -67,23 +74,106 @@ describe('<ActiveFilters>', () => {
 		});
 	});
 
-	//TODO If I got time. Else, submit a US.
-	describe.skip('AND/OR', () => {
-		it('has a button that allows showing a text input when it his hidden');
-		it('has a button to remove the text input when it is shown');
+	describe.only('AND/OR', () => {
+		it('has a button to show the AND/OR logic input', () => {
+			cy.mountWithStore(<ActiveFilters />);
+
+			cy.getBySel(hideAndOrButtonSelector).should('not.exist');
+			cy.getBySel(showAndOrButtonSelector).should('exist');
+		});
+
+		it('applies stored AND/OR logic to the cbql string when the input is toggled to show', () => {
+			const store = getStore();
+			const andOrLogic = '1 OR 2';
+			store.dispatch(setAndOrFilter(andOrLogic));
+
+			cy.spy(store, 'dispatch').as('dispatch');
+
+			cy.mountWithStore(<ActiveFilters />, { reduxStore: store });
+
+			cy.getBySel(showAndOrButtonSelector).click();
+
+			cy.get('@dispatch').then((dispatch) =>
+				expect(dispatch).to.have.been.calledWith(
+					setAndOrFilter(andOrLogic)
+				)
+			);
+		});
+
+		it('has a button to remove the AND/OR logic input when it is shown', () => {
+			const store = getStore();
+			store.dispatch(setAndOrFilterEnabled(true));
+
+			cy.mountWithStore(<ActiveFilters />, { reduxStore: store });
+
+			cy.getBySel(showAndOrButtonSelector).should('not.exist');
+			cy.getBySel(hideAndOrButtonSelector).should('exist');
+		});
+
+		it('exempts AND/OR logic from the cbql string when the input is toggled to hide', () => {
+			const store = getStore();
+			store.dispatch(setAndOrFilterEnabled(true));
+
+			cy.spy(store, 'dispatch').as('dispatch');
+
+			cy.mountWithStore(<ActiveFilters />, { reduxStore: store });
+
+			cy.getBySel(hideAndOrButtonSelector).click();
+
+			cy.get('@dispatch').then((dispatch) =>
+				expect(dispatch).to.have.been.calledWith(
+					resetCbqlStringToCurrentParameters()
+				)
+			);
+		});
+
+		it('shows filters their Id(+1)s when showing the AND/OR logic input', () => {
+			const store = getStore();
+			store.dispatch(setAndOrFilterEnabled(true));
+			for (let i = 0; i < testCriteria.length; i++) {
+				store.dispatch(addFilter(testCriteria[i]));
+			}
+
+			cy.mountWithStore(<ActiveFilters />, { reduxStore: store });
+
+			for (let i = 0; i < testCriteria.length; i++) {
+				cy.getBySel(`criteria-${testCriteria[i].id}`)
+					.should('exist')
+					.and('contain.text', (testCriteria[i].id! + 1).toString());
+			}
+		});
 
 		describe('Input', () => {
+			it('updates the stored AND/OR string when submitting', () => {
+				const store = getStore();
+				store.dispatch(setAndOrFilterEnabled(true));
+				const andOrLogic = '(1 AND (2 OR 3))';
+
+				cy.spy(store, 'dispatch').as('dispatch');
+
+				cy.mountWithStore(<ActiveFilters />, { reduxStore: store });
+
+				cy.getBySel(andOrInputSelector)
+					.clear()
+					.type(`${andOrLogic}{enter}`);
+
+				cy.get('@dispatch').then((dispatch) =>
+					expect(dispatch).to.have.been.calledWith(
+						setAndOrFilter(andOrLogic)
+					)
+				);
+			});
+
+			//* nice to have (but a struggle to implement)
 			it('validates the input to only contain the valid characters'); //being AND, OR and brackets
 			it(
 				'validates the input to only contain numbers that have respective active filter criteria'
 			); //by their id(+1)
-
 			it('displays the input text in green when it is valid');
 			it('displays the input text in red when it is invalid');
-
-			it(
-				'updates the stored AND/OR string, with filter IDs replaced by their respective CBQL, when submitting'
-			);
 		});
+	});
+	afterEach(() => {
+		localStorage.clear();
 	});
 });
