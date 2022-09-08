3e03007a05fd69ad7a34a1ae3d74a3a1139054f8
urecha <70019423+urecha@users.noreply.github.com>
2022-09-08 07:04:24 +0200

    update QueryResult component
    add checked and disabled props, update data-test attr

--- src/pages/import/components/queryResult/QueryResult.tsx
+++ src/pages/import/components/queryResult/QueryResult.tsx
@@ -3,6 +3,8 @@ import { ItemListView } from '../../../../models/itemListView';
 
 export default function QueryResult(props: {
 	item: ItemListView;
+	checked?: boolean;
+	disabled?: boolean;
 	onSelect: Function;
 }) {
 	return (
@@ -11,10 +13,12 @@ export default function QueryResult(props: {
 				<input
 					type="checkbox"
 					className="checkBox"
+					checked={props.checked}
 					onChange={(e) =>
 						props.onSelect(props.item, e.target.checked)
 					}
-					data-test="itemCheck"
+					disabled={props.disabled}
+					data-test={'itemCheck-' + props.item.id}
 				></input>
 			</td>
 			<td data-test="itemId">{props.item.id}</td>
