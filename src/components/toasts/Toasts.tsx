344749576e492e46c53a14e3fa85cf5617b53371
urecha <70019423+urecha@users.noreply.github.com>
2022-09-12 07:00:31 +0200

    implement Toasts component

--- src/components/toasts/Toasts.tsx
+++ src/components/toasts/Toasts.tsx
@@ -1,5 +1,39 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../../store/store';
+
+import ToastContainer from 'react-bootstrap/ToastContainer';
+import { Toast, ToastHeader } from 'react-bootstrap';
+import { removeAppMessage } from '../../store/slices/appMessagesSlice';
 
 export default function Toasts() {
-	return <div>Toasts</div>;
+	const dispatch = useDispatch();
+	const { messages } = useSelector((store: RootState) => store.appMessages);
+
+	const removeToast = (messageId: number) => {
+		console.log('Removing toast: ', messageId);
+		dispatch(removeAppMessage(messageId));
+	};
+
+	const DEFAULT_DELAY = 5000;
+
+	return (
+		<ToastContainer position="bottom-end">
+			{messages.map((m) => (
+				<Toast
+					bg={m.bg}
+					autohide={true}
+					delay={m.delay ?? DEFAULT_DELAY}
+					onClose={() => removeToast(m.id!)}
+					key={`${m.id}-${m.header}`} //ids are reused
+					data-test={`toast-${m.id}`}
+				>
+					<ToastHeader>{m.header}</ToastHeader>
+					<Toast.Body className={m.bg === 'dark' ? 'text-white' : ''}>
+						{m.content}
+					</Toast.Body>
+				</Toast>
+			))}
+		</ToastContainer>
+	);
 }
