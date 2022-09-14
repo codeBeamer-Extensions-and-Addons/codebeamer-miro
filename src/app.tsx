5af8b62f8e5501c9b661f8e2eb826a7ef3d3bf29
urecha <70019423+urecha@users.noreply.github.com>
2022-09-14 09:10:58 +0200

    Update app.tsx
    - add Toasts element
    - add additional commented-out dev reset statements

--- src/app.tsx
+++ src/app.tsx
@@ -5,21 +5,27 @@ import { Provider } from 'react-redux';
 import { store } from './store/store';
 
 import Content from './pages/content/Content';
+import Toasts from './components/toasts/Toasts';
 
 function App() {
 	//* manual projectId reset because I haven't implemented it yet in the UI
-	// React.useEffect(() => {
-	// miro.board.setAppData('projectId', '');
-	// miro.board.setAppData('cbAddress', '');
-	// miro.board.setAppData(
-	// 	'cardTagConfiguration',
-	// 	'{ standard: {}, trackerSpecific: {} }'
-	// );
-	// });
+	React.useEffect(() => {
+		// miro.board.setAppData('projectId', '');
+		// miro.board.setAppData('cbAddress', '');
+		// const config = {
+		// 	standard: {},
+		// 	trackerSpecific: [],
+		// };
+		// console.log(config);
+		// miro.board.setAppData('cardTagConfiguration', config);
+		// localStorage.clear();
+		// sessionStorage.clear();
+	});
 
 	return (
 		<Provider store={store}>
 			<Content />
+			<Toasts />
 		</Provider>
 	);
 }
