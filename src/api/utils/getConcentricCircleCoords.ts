ea249e6e92bbd3c5dab381458f5894f5adbb7a34
urecha <70019423+urecha@users.noreply.github.com>
2022-09-24 10:35:07 +0200

    Create getConcentricCircleCoords.ts
    function returns coordinates for items so that they're grouped around their subject
    the subject origins are aligned in concentric circles around the viewport's center

---
+++ src/api/utils/getConcentricCircleCoords.ts
@@ -0,0 +1,104 @@
+import { Rect } from '@mirohq/websdk-types';
+import {
+	DEFAULT_ORIGINS_PER_RADIUS,
+	RADIUS_INCREMENT,
+	MAX_OFFSET_TO_SUBJECT_ORIGIN,
+} from '../../constants/coordinate-calculation';
+import { CodeBeamerItem } from '../../models/codebeamer-item.if';
+import getRandomOffset from './getRandomOffset';
+
+var subjectOrigins: { subject: string; x: number; y: number }[] = [];
+var currentViewPort: Rect;
+var circleXCenter: number;
+var circleYCenter: number;
+
+let itemIndex = 1;
+let radiusIndex = 1;
+
+/**
+ * Calculates the allowed number of origins for the current radius increment
+ * @param radiusIndex The current radius index
+ * @returns Number of allowed origins for this radius.
+ */
+function getAllowedNoOfOriginsForRadius(radiusIndex: number) {
+	return DEFAULT_ORIGINS_PER_RADIUS * radiusIndex;
+}
+
+/**
+ * Creates coordinates that makes Items spawn grouped by their subjects in concentric circles.
+ * <p>
+ * Items without a subject are put into the circle's center. For each subject thereafter, an origin
+ * coordinate set is created in concentric circles. The first circle has {@link DEFAULT_ORIGINS_PER_RADIUS} subjects,
+ * the others {radiusIndex * the Default origins}.
+ * </p>
+ * <p>
+ * Distribution and therefore clarity are heavily impacted by the values of
+ * {@link DEFAULT_ORIGINS_PER_RADIUS}, {@link RADIUS_INCREMENT} and {@link MAX_OFFSET_TO_SUBJECT_ORIGIN}
+ * </p>
+ * @param item
+ * @returns An {x,y} coordinate set that groups the Item with others of the same subject.
+ */
+export default async function getConcentricCircleCoords(
+	item: CodeBeamerItem
+): Promise<{ x: number; y: number }> {
+	//* just do it once
+	if (!currentViewPort) currentViewPort = await miro.board.viewport.get();
+	//* initialize default subject
+	if (!subjectOrigins.length) {
+		circleXCenter = currentViewPort.x + currentViewPort.width / 2;
+		circleYCenter = currentViewPort.y + currentViewPort.height / 2;
+		subjectOrigins.push({
+			subject: 'none',
+			x: circleXCenter,
+			y: circleYCenter,
+		});
+	}
+
+	let itemSubject = item.subjects[0]?.name ?? 'none';
+	let subjectOrigin = subjectOrigins.find((so) => so.subject == itemSubject);
+
+	if (!subjectOrigin) {
+		let index = itemIndex++;
+		//* for a snail form instead of a concentric circle, uncomment following line and remove the radiusIndex incrementer at the if's end
+		// let radiusIndex = Math.ceil(index / DEFAULT_ORIGINS_PER_RADIUS);
+		let x =
+			circleXCenter +
+			Math.cos(
+				2 *
+					Math.PI *
+					((index -
+						(radiusIndex -
+							1 *
+								getAllowedNoOfOriginsForRadius(
+									radiusIndex - 1
+								))) /
+						getAllowedNoOfOriginsForRadius(radiusIndex))
+			) *
+				(RADIUS_INCREMENT * radiusIndex);
+		let y =
+			circleYCenter +
+			Math.sin(
+				2 *
+					Math.PI *
+					((index -
+						(radiusIndex -
+							1 *
+								getAllowedNoOfOriginsForRadius(
+									radiusIndex - 1
+								))) /
+						getAllowedNoOfOriginsForRadius(radiusIndex))
+			) *
+				(RADIUS_INCREMENT * radiusIndex);
+		subjectOrigin = { subject: itemSubject, x: x, y: y };
+		subjectOrigins.push(subjectOrigin);
+		if (index / getAllowedNoOfOriginsForRadius(radiusIndex) >= 1) {
+			itemIndex = 1;
+			radiusIndex++; //* for a snail form instead, remove this
+		}
+	}
+
+	return {
+		x: subjectOrigin.x + getRandomOffset(MAX_OFFSET_TO_SUBJECT_ORIGIN),
+		y: subjectOrigin.y + getRandomOffset(MAX_OFFSET_TO_SUBJECT_ORIGIN),
+	};
+}
