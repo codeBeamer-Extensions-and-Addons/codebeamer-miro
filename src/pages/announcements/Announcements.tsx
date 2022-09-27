e21fbb0415c25b7be4615a1ac2cfbcca77679ac2
urecha <70019423+urecha@users.noreply.github.com>
2022-09-27 09:30:03 +0200

    add announcements.tsx boilerplate

---
+++ src/pages/announcements/Announcements.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+/**
+ * Container for (a) page(s) containing announcements.
+ */
+export default function Announcements() {
+	return (
+		<Swiper>
+			<SwiperSlide></SwiperSlide>
+		</Swiper>
+	);
+}
