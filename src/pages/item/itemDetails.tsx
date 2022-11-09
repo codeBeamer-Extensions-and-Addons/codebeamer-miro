6035ba2a44ce60647ec1170ddfc6a7b3c802b867
urecha <70019423+urecha@users.noreply.github.com>
2022-11-09 14:16:45 +0100

    update / refactor itemDetailts.tsx
    WIP
    add some boilerplate, load the item's data

--- src/pages/item/itemDetails.tsx
+++ src/pages/item/itemDetails.tsx
@@ -1,40 +1,79 @@
-import { Field, Formik } from 'formik';
 import React, { useState } from 'react';
-import Select from 'react-select';
-import { EDITABLE_ATTRIBUTES } from '../../constants/editable-attributes';
+import { useDispatch, useSelector } from 'react-redux';
+import { useLazyGetItemQuery } from '../../api/codeBeamerSwaggerApi';
 import { CodeBeamerItem } from '../../models/codebeamer-item.if';
+import { loadBoardSettings } from '../../store/slices/boardSettingsSlice';
+import { RootState } from '../../store/store';
+
+//It's 367 wide, really.
+const PANEL_WIDTH = 500;
 
 export default function ItemDetails() {
-	//It's 367 wide, really.
-	const PANEL_WIDTH = 500;
+	const dispatch = useDispatch();
 
+	const { cbAddress } = useSelector(
+		(state: RootState) => state.boardSettings
+	);
+	const [storeIsInitializing, setStoreIsInitializing] =
+		useState<boolean>(true);
 	const [item, setItem] = useState<CodeBeamerItem>();
-	const [itemId, setItemId] = useState<string>();
-	const [cardId, setCardId] = useState<string>();
-	const [selectOptions, setSelectOptions] = useState<
-		{
-			attribute: string;
-			options: { name: string; id: string }[];
-		}[]
-	>();
+
+	const searchParams = new URL(document.location.href).searchParams;
+	console.log(document.location.href);
+
+	const itemId = searchParams.get('itemId');
+	if (!itemId) {
+		//TODO
+	}
+	const cardId = searchParams.get('cardId');
 
 	React.useEffect(() => {
-		const searchParams = new URL(document.location.href).searchParams;
-		console.log(document.location.href);
-		setItemId(searchParams.get('itemId') ?? '');
-		setCardId(searchParams.get('cardId') ?? '');
+		console.log('Dispatchign board setting load');
+		dispatch(loadBoardSettings());
+		if (cbAddress) {
+			console.log('Cb address truthy: ', cbAddress);
+			setStoreIsInitializing(false);
+			trigger(itemId!);
+		}
 	}, []);
+
+	React.useEffect(() => {
+		console.log('cbAddress effect');
+		if (cbAddress) {
+			setStoreIsInitializing(false);
+			trigger(itemId!);
+		}
+	}, [cbAddress]);
+
+	const [trigger, result, lastPromiseInfo] = useLazyGetItemQuery();
+
+	React.useEffect(() => {
+		if (result.error) {
+			//TODO
+		} else if (result.data) {
+			setItem(result.data);
+		}
+	}, [result]);
 
 	const updateValues = (attr: string, value: any) => {
 		console.log(`Update ${attr} to ${value}`);
 	};
 
 	return (
-		<div className="centered-horizontally">
-			<h3 className="h3">
-				Item {itemId} / Widget {cardId}
-			</h3>
-			<div className="grid"></div>
-		</div>
+		<>
+			{storeIsInitializing ||
+				(result.isLoading && (
+					<div className="centered loading-spinner"></div>
+				))}
+			<div className="fade-in centered-horizontally">
+				<h3 className="h3">
+					Item {itemId} / Widget {cardId}
+				</h3>
+				<div className="grid">
+					<label>Item:</label>
+					{item && <p>{JSON.stringify(item)}</p>}
+				</div>
+			</div>
+		</>
 	);
 }
