9310749c9b39f02ee40c66a4a317d8cbbb378fda
urecha <70019423+urecha@users.noreply.github.com>
2022-11-10 09:07:37 +0100

    update index.ts
    remove obsolete code
    add console.info on app_card:connect

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,4 @@
-import { convertToCardData, updateAppCard } from './api/miro.api';
 import getItemIdFromCardTitle from './api/utils/getItemIdFromCardTitle';
-import { CARD_TITLE_ID_FILTER_REGEX } from './constants/cardTitleIdFilterRegex';
-import { CodeBeamerItem } from './models/codebeamer-item.if';
-import TrackerDetails from './models/trackerDetails.if';
-import { BoardSetting } from './store/enums/boardSetting.enum';
-import { UserSetting } from './store/enums/userSetting.enum';
-import { getStore } from './store/store';
 
 async function init() {
 	miro.board.ui.on('icon:click', async () => {
@@ -25,80 +18,17 @@ async function init() {
 		});
 
 		return;
-		//* experimental, just to have something there.
-		//* except that codeBeamer has x-fram-origin = samesite
-
-		console.log('AppCardOpen Event: ', _event);
-		const { appCard } = _event;
-		const href = appCard.title.match(/(href\=\")[a-zA-Z0-9\.\/\:]+(\")/);
-		if (!href?.length) {
-			//TODO miro showErrorNotif
-			console.error('No URL match on the item');
-			return;
-		}
-		const url = href[0].substring(6, href[0].length - 1);
-
-		if (url.includes('localhost') || url.includes('github')) {
-			//TODO miro.showErrorNotif
-			console.warn(
-				"Can't open App Card Details: Invalid CodeBeamer Address - Is it not configured?"
-			);
-			return;
-		}
-
-		miro.board.ui.openModal({
-			url,
-		});
 	});
 
 	miro.board.ui.on('app_card:connect', async (_event) => {
+		const { appCard } = _event;
+		appCard.status = 'connected';
+		appCard.sync();
+
 		const message =
-			"This feature is not implemented. You can update Items with the 'Sync' button in the Plugin's UI.";
-		console.warn(message);
-		//TODO miro.showNotification("message")
-
-		//* Sync the item (once).
-		// let { appCard } = _event;
-
-		// const cbBaseUrl = await miro.board.getAppData(BoardSetting.CB_ADDRESS);
-		// const username = localStorage.getItem(UserSetting.CB_USERNAME);
-		// const trackerId = localStorage.getItem(UserSetting.SELECTED_TRACKER);
-		// const password = sessionStorage.getItem(UserSetting.CB_PASSWORD);
-
-		// const itemKey = appCard.title.match(CARD_TITLE_ID_FILTER_REGEX);
-
-		// if (!itemKey?.length) {
-		// 	//TODO miro showErrorNotif
-		// 	console.warn("Couldn't extract ID from Card title. Can't sync!");
-		// 	return;
-		// }
-		// const itemId = itemKey[1];
-
-		// const requestArgs = {
-		// 	method: 'GET',
-		// 	headers: new Headers({
-		// 		'Content-Type': 'application/json',
-		// 		Authorization: `Basic ${btoa(username + ':' + password)}`,
-		// 	}),
-		// };
-
-		// const cbItem = (await (
-		// 	await fetch(`${cbBaseUrl}/api/v3/items/${itemId}`, requestArgs)
-		// ).json()) as CodeBeamerItem;
-
-		// const trackerData = (await (
-		// 	await fetch(
-		// 		`${cbBaseUrl}/api/v3/trackers/${trackerId}`,
-		// 		requestArgs
-		// 	)
-		// ).json()) as TrackerDetails;
-
-		// cbItem.tracker.keyName = trackerData.keyName;
-		// cbItem.tracker.color = trackerData.color;
-
-		// const store = getStore();
-		//! store doesn't contain the expected data
-		// updateAppCard(cbItem, appCard.id, false, store);
+			"Clicking the 'Connect' button does not establish a persistent connection or update the item's data. " +
+			"But it allows to now open the Item's details (which updates it too) with the icon that appeared in the same place.";
+		console.info(message);
 	});
 
 	console.info(
