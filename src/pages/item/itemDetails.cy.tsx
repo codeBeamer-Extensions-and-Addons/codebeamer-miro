0930df93de5007fe7d62b971258c366338435327
urecha <70019423+urecha@users.noreply.github.com>
2022-11-10 08:40:49 +0100

    update itemDetails component & tests
    add form elements
    implement & add some tests
    
    WIP

--- src/pages/item/itemDetails.cy.tsx
+++ src/pages/item/itemDetails.cy.tsx
@@ -1,6 +1,15 @@
 import * as React from 'react';
 import ItemDetails from './itemDetails';
 import { EDITABLE_ATTRIBUTES } from '../../constants/editable-attributes';
+import { getStore } from '../../store/store';
+import { setCbAddress } from '../../store/slices/boardSettingsSlice';
+
+const getSelectControlSelector = (labelDataAttribute: String) => {
+	return `[data-test=${labelDataAttribute}] + div .select__control`;
+};
+
+const mockItemId = '201284';
+const mockCardId = '30642';
 
 describe('<Item>', () => {
 	it('mounts', () => {
@@ -8,19 +17,82 @@ describe('<Item>', () => {
 	});
 
 	it('updates its card its data when opened', () => {
-		cy.mountWithStore(<ItemDetails />);
+		const mockCbAddress = 'http://test.cb.net/cb';
+		const store = getStore();
+		store.dispatch(setCbAddress(mockCbAddress));
+
+		cy.intercept(`${mockCbAddress}/api/v3/items/${mockItemId}`, {
+			fixture: 'item.json',
+		}).as('fetchItem');
+		cy.intercept(`**wiki2html`, { statusCode: 200 });
+
+		cy.mountWithStore(
+			<ItemDetails itemId={mockItemId} cardId={mockCardId} />,
+			{
+				reduxStore: store,
+			}
+		);
+
+		cy.wait('@fetchItem');
 	});
 
-	it.only('has an input for each editable attribute', () => {
+	it('has an input for each editable attribute', () => {
 		cy.mountWithStore(<ItemDetails />);
 
 		for (let attr of EDITABLE_ATTRIBUTES) {
+			//*mind that the data-test attributes are on labels for selects
 			cy.getBySel(attr.name).should('exist');
 		}
 	});
 
+	context('form interaction', () => {
+		describe('assignee input', () => {
+			it('has no options initially', () => {
+				cy.mountWithStore(<ItemDetails />);
+				cy.get(getSelectControlSelector('assignedTo')).should(
+					'not.contain.html',
+					'option'
+				);
+			});
+			it.only('loads suitable options once typing starts', () => {
+				const store = getStore();
+				const mockCbAddress = 'http://test.com/cb';
+				store.dispatch(setCbAddress(mockCbAddress));
+
+				cy.mountWithStore(
+					<ItemDetails itemId={mockItemId} cardId={mockCardId} />,
+					{ reduxStore: store }
+				);
+				cy.intercept(`/rest/users/page/*`, {
+					fixture: 'users_ur.json',
+				});
+
+				cy.get(getSelectControlSelector('assignedTo'))
+					.type('ur')
+					.then(() => {
+						cy.fixture('users_ur.json').then((res) => {
+							const users = res.users;
+							let select = cy.get(
+								getSelectControlSelector('assignedTo')
+							);
+							for (let user of users) {
+								select.should('contain.text', user.name);
+							}
+						});
+					});
+			});
+		});
+
+		describe('teams/version/subject input', () => {
+			it('shows items in the respectively referred tracker as options');
+		});
+	});
+
 	context('making updates', () => {
-		it('has a button to submit changes');
+		it('has a button to submit changes', () => {
+			cy.mountWithStore(<ItemDetails />);
+			cy.getBySel('submit').should('exist');
+		});
 
 		it('updates the codeBeamer source item when submitting updates');
 
--- src/pages/item/itemDetails.tsx
+++ src/pages/item/itemDetails.tsx
@@ -1,6 +1,11 @@
+import { Field, useFormik } from 'formik';
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import Select from 'react-select';
+import AsyncSelect from 'react-select/async';
+import { useLazyGetUsersQuery } from '../../api/codeBeamerRestApi';
 import { useLazyGetItemQuery } from '../../api/codeBeamerSwaggerApi';
+import { updateAppCard } from '../../api/miro.api';
 import { CodeBeamerItem } from '../../models/codebeamer-item.if';
 import { loadBoardSettings } from '../../store/slices/boardSettingsSlice';
 import { RootState } from '../../store/store';
@@ -8,71 +13,289 @@ import { RootState } from '../../store/store';
 //It's 367 wide, really.
 const PANEL_WIDTH = 500;
 
-export default function ItemDetails() {
-	const dispatch = useDispatch();
+interface Errors {
+	assignedTo?: string;
+	teams?: string;
+	storyPoints?: string;
+	versions?: string;
+	subjects?: string;
+}
 
-	const { cbAddress } = useSelector(
-		(state: RootState) => state.boardSettings
+/**
+ * @param props Props are only used in testing - the values are passed via url query params else.
+ */
+export default function ItemDetails(props: {
+	itemId?: string;
+	cardId?: string;
+}) {
+	const dispatch = useDispatch();
+	const [itemId] = useState<string>(
+		props.itemId ??
+			new URL(document.location.href).searchParams.get('itemId') ??
+			''
+	);
+	const [cardId] = useState<string>(
+		props.cardId ??
+			new URL(document.location.href).searchParams.get('cardId') ??
+			''
 	);
 	const [storeIsInitializing, setStoreIsInitializing] =
 		useState<boolean>(true);
 	const [item, setItem] = useState<CodeBeamerItem>();
 
-	const searchParams = new URL(document.location.href).searchParams;
-	console.log(document.location.href);
+	const { cbAddress } = useSelector(
+		(state: RootState) => state.boardSettings
+	);
 
-	const itemId = searchParams.get('itemId');
-	if (!itemId) {
-		//TODO
-	}
-	const cardId = searchParams.get('cardId');
+	const [triggerItemQuery, itemQueryResult, itemQueryLastPromiseInfo] =
+		useLazyGetItemQuery();
+	const [triggerUserQuery, userQueryResult, userQueryLastPromiseInfo] =
+		useLazyGetUsersQuery();
 
 	React.useEffect(() => {
-		console.log('Dispatchign board setting load');
+		if (!itemId || !cardId) {
+			console.error(
+				'Item page called without itemId and/or cardId in query.'
+			);
+			return;
+		}
+		// console.log('Dispatchign board setting load');
 		dispatch(loadBoardSettings());
 		if (cbAddress) {
-			console.log('Cb address truthy: ', cbAddress);
+			// console.log('Cb address truthy: ', cbAddress);
 			setStoreIsInitializing(false);
-			trigger(itemId!);
+			triggerItemQuery(itemId!);
 		}
 	}, []);
 
 	React.useEffect(() => {
-		console.log('cbAddress effect');
+		// console.log('cbAddress effect');
 		if (cbAddress) {
 			setStoreIsInitializing(false);
-			trigger(itemId!);
+			triggerItemQuery(itemId!);
 		}
 	}, [cbAddress]);
 
-	const [trigger, result, lastPromiseInfo] = useLazyGetItemQuery();
-
 	React.useEffect(() => {
-		if (result.error) {
+		if (itemQueryResult.error) {
 			//TODO
-		} else if (result.data) {
-			setItem(result.data);
+		} else if (itemQueryResult.data) {
+			setItem(itemQueryResult.data);
+			updateAppCard(itemQueryResult.data, cardId);
 		}
-	}, [result]);
+	}, [itemQueryResult]);
 
 	const updateValues = (attr: string, value: any) => {
 		console.log(`Update ${attr} to ${value}`);
 	};
 
+	const fetchUsers = (
+		inputValue: string,
+		callback: (options: any) => void
+	) => {
+		triggerUserQuery(inputValue);
+		console.log('Triggered user query..');
+		while (userQueryResult.isFetching || !userQueryResult.data) {
+			if (userQueryResult.error) {
+				console.log(userQueryResult.error);
+				break;
+			}
+		}
+		console.log('No longer fetching..');
+		callback(userQueryResult.data);
+	};
+
+	const formik = useFormik({
+		initialValues: {
+			assignedTo: [],
+			teams: [],
+			storyPoints: -1,
+			versions: [],
+			subjects: [],
+		},
+		enableReinitialize: true,
+		validate: (values) => {
+			const errors: Errors = {};
+
+			return errors;
+		},
+		onSubmit: (values, { setSubmitting }) => {
+			setSubmitting(true);
+			//TODO trigger POST
+		},
+	});
+
 	return (
 		<>
 			{storeIsInitializing ||
-				(result.isLoading && (
+				(itemQueryResult.isLoading && (
 					<div className="centered loading-spinner"></div>
 				))}
 			<div className="fade-in centered-horizontally">
 				<h3 className="h3">
 					Item {itemId} / Widget {cardId}
 				</h3>
-				<div className="grid">
-					<label>Item:</label>
-					{item && <p>{JSON.stringify(item)}</p>}
-				</div>
+
+				<form onSubmit={formik.handleSubmit}>
+					<div
+						className={`form-group ${
+							formik.touched.assignedTo
+								? formik.errors.assignedTo
+									? 'error'
+									: 'success'
+								: ''
+						}`}
+					>
+						<label data-test="assignedTo">Assignee</label>
+						<AsyncSelect
+							className="basic-single"
+							classNamePrefix="select"
+							defaultOptions
+							loadOptions={fetchUsers}
+							isLoading={false}
+							isMulti={true}
+							isSearchable={true}
+							isClearable={true}
+							onChange={(v) => {
+								console.log(v);
+							}}
+							maxMenuHeight={180}
+						/>
+					</div>
+					<div
+						className={`form-group ${
+							formik.touched.teams
+								? formik.errors.teams
+									? 'error'
+									: 'success'
+								: ''
+						}`}
+					>
+						<label data-test="teams">Teams</label>
+						<Select
+							className="basic-single"
+							classNamePrefix="select"
+							options={[]}
+							isLoading={false}
+							isMulti={true}
+							isSearchable={true}
+							isClearable={true}
+							getOptionLabel={(option) => option}
+							getOptionValue={(option) => option}
+							onChange={(v) => {
+								console.log(v);
+							}}
+							maxMenuHeight={180}
+						/>
+					</div>
+					<div
+						className={`form-group ${
+							formik.touched.versions
+								? formik.errors.versions
+									? 'error'
+									: 'success'
+								: ''
+						}`}
+					>
+						<label data-test="versions">Version</label>
+						<Select
+							className="basic-single"
+							classNamePrefix="select"
+							options={[]}
+							isLoading={false}
+							isMulti={true}
+							isSearchable={true}
+							isClearable={true}
+							getOptionLabel={(option) => option}
+							getOptionValue={(option) => option}
+							onChange={(v) => {
+								console.log(v);
+							}}
+							maxMenuHeight={180}
+						/>
+					</div>
+					<div
+						className={`form-group ${
+							formik.touched.subjects
+								? formik.errors.subjects
+									? 'error'
+									: 'success'
+								: ''
+						}`}
+					>
+						<label data-test="subjects">Subject</label>
+						<Select
+							className="basic-single"
+							classNamePrefix="select"
+							options={[]}
+							isLoading={false}
+							isMulti={true}
+							isSearchable={true}
+							isClearable={true}
+							getOptionLabel={(option) => option}
+							getOptionValue={(option) => option}
+							onChange={(v) => {
+								console.log(v);
+							}}
+							maxMenuHeight={180}
+						/>
+					</div>
+					<div
+						className={`form-group ${
+							formik.touched.storyPoints
+								? formik.errors.storyPoints
+									? 'error'
+									: 'success'
+								: ''
+						}`}
+					>
+						<label>Story Points</label>
+						<input
+							type="number"
+							name="storyPoints"
+							value={formik.values.storyPoints}
+							className="input"
+							onChange={(e) => formik.handleChange(e)}
+							data-test="storyPoints"
+						/>
+					</div>
+					<div className="flex-centered mt-4">
+						{true && (
+							<button
+								type="submit"
+								disabled={formik.isSubmitting}
+								data-test="submit"
+								className={`fade-in button button-primary ${
+									formik.isSubmitting ? 'button-loading' : ''
+								}`}
+							>
+								Save
+							</button>
+						)}
+						{false && (
+							<span>
+								<svg
+									className="checkmark"
+									xmlns="http://www.w3.org/2000/svg"
+									viewBox="0 0 52 52"
+								>
+									<circle
+										className="checkmark__circle"
+										cx="26"
+										cy="26"
+										r="25"
+										fill="none"
+									/>
+									<path
+										className="checkmark__check"
+										fill="none"
+										d="M14.1 27.2l7.1 7.2 16.7-16.8"
+									/>
+								</svg>
+							</span>
+						)}
+					</div>
+				</form>
 			</div>
 		</>
 	);
