ab79affd0663afe97c2795b6c2eea64e2948a45f
urecha <70019423+urecha@users.noreply.github.com>
2022-11-10 13:15:58 +0100

    refactorings api query interfaces
    combine similar interfaces in api-query-types
    (all share "page","pagesize","total", and only differ in the
    "values" key name)

---
+++ src/models/api-query-types.ts
@@ -0,0 +1,25 @@
+export interface ItemQueryPage {
+	page: number;
+	pageSize: number;
+	total: number;
+	items: any[];
+}
+
+export interface TrackerSearchPage {
+	page: number;
+	pageSize: number;
+	total: number;
+	trackers: any[];
+}
+
+export interface UserQueryPage {
+	page: number;
+	pageSize: number;
+	total: number;
+	users: {
+		name: string;
+		firstName: string;
+		lastName: string;
+		email: string;
+	}[];
+}
--- src/models/itemQueryPage.ts
+++
@@ -1,6 +0,0 @@
-export interface ItemQueryPage {
-	page: number;
-	pageSize: number;
-	total: number;
-	items: any[];
-}
