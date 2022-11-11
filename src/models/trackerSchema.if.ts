f3569d81d5a448869b6f7d21b04e548d78173df9
urecha <70019423+urecha@users.noreply.github.com>
2022-11-11 07:44:24 +0100

    update CodeBeamerTrackerSchemaEntry if
    add prop id

--- src/models/trackerSchema.if.ts
+++ src/models/trackerSchema.if.ts
@@ -2,6 +2,7 @@
  * Notworthy properties of an entry in a Tracker's schema array
  */
 export interface CodeBeamerTrackerSchemaEntry {
+	id: number;
 	name: string;
 	legacyRestName: string;
 	trackerItemField: string;
