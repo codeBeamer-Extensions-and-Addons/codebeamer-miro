be3ecf56e7e02bdf7484c99a6c0edafabd8d9561
urecha <70019423+urecha@users.noreply.github.com>
2022-11-14 18:02:31 +0100

    add rtkQueryErrorLogger.ts
    add global API error handler
    toasts errors (to be replaced with miro error notifications)

---
+++ src/api/rtkQueryErrorLogger.ts
@@ -0,0 +1,29 @@
+import {
+	isRejectedWithValue,
+	Middleware,
+	MiddlewareAPI,
+} from '@reduxjs/toolkit';
+import { displayAppMessage } from '../store/slices/appMessagesSlice';
+import { store } from '../store/store';
+
+export const rtkQueryErrorLogger: Middleware =
+	(api: MiddlewareAPI) => (next) => (action) => {
+		if (isRejectedWithValue(action)) {
+			if (action.payload?.data?.message == 'Not authenticated') {
+				//* just ignore it. would appear every time you open the app, but doesn't need this kind of communication
+				next(action);
+			}
+			console.error('Error action: ', action);
+			store.dispatch(
+				displayAppMessage({
+					header: 'Error',
+					content:
+						action.payload.data.message ??
+						'An error happend! Check the console for potential details.',
+					bg: 'danger',
+					delay: 3500,
+				})
+			);
+		}
+		next(action);
+	};
