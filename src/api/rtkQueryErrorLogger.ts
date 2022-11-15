0dc94f830200264709405afee5a952ee8b97b13c
urecha <70019423+urecha@users.noreply.github.com>
2022-11-15 06:18:51 +0100

    fix access property of undef in rtkQueryErrorLogger
    add the necessary null/undefined checks

--- src/api/rtkQueryErrorLogger.ts
+++ src/api/rtkQueryErrorLogger.ts
@@ -9,17 +9,20 @@ import { store } from '../store/store';
 export const rtkQueryErrorLogger: Middleware =
 	(api: MiddlewareAPI) => (next) => (action) => {
 		if (isRejectedWithValue(action)) {
-			if (action.payload?.data?.message == 'Not authenticated') {
+			if (
+				action.payload?.data?.message &&
+				action.payload.data.message == 'Not authenticated'
+			) {
 				//* just ignore it. would appear every time you open the app, but doesn't need this kind of communication
 				next(action);
 			}
-			console.error('Error action: ', action);
+			console.error('Error in API query - Action: ', action);
 			store.dispatch(
 				displayAppMessage({
 					header: 'Error',
 					content:
-						action.payload.data.message ??
-						'An error happend! Check the console for potential details.',
+						action.payload?.data?.message ??
+						'An error occured! Check the console for potential details.',
 					bg: 'danger',
 					delay: 3500,
 				})
