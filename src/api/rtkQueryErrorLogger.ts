a04679fbf490837f3931bb4e84dec7bdc1c9a5eb
urecha <70019423+urecha@users.noreply.github.com>
2022-11-15 06:40:04 +0100

    minor refactor rtkQueryErrorLogger
    refactor if-statement and add return where it should return

--- src/api/rtkQueryErrorLogger.ts
+++ src/api/rtkQueryErrorLogger.ts
@@ -9,12 +9,9 @@ import { store } from '../store/store';
 export const rtkQueryErrorLogger: Middleware =
 	(api: MiddlewareAPI) => (next) => (action) => {
 		if (isRejectedWithValue(action)) {
-			if (
-				action.payload?.data?.message &&
-				action.payload.data.message == 'Not authenticated'
-			) {
+			if (action.payload?.status == 401) {
 				//* just ignore it. would appear every time you open the app, but doesn't need this kind of communication
-				next(action);
+				return next(action);
 			}
 			console.error('Error in API query - Action: ', action);
 			store.dispatch(
@@ -24,7 +21,7 @@ export const rtkQueryErrorLogger: Middleware =
 						action.payload?.data?.message ??
 						'An error occured! Check the console for potential details.',
 					bg: 'danger',
-					delay: 3500,
+					delay: 5000,
 				})
 			);
 		}
