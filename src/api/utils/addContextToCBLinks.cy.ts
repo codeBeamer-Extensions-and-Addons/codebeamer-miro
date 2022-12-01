31648641096d1e3b1de84c969b3a8e5657d7519e
urecha <70019423+urecha@users.noreply.github.com>
2022-12-01 09:14:56 +0100

    fix addContextToCBLinks test
    update expetced value to contain target="_blank"

--- src/api/utils/addContextToCBLinks.cy.ts
+++ src/api/utils/addContextToCBLinks.cy.ts
@@ -6,7 +6,7 @@ describe('addContextToCBLinks', () => {
 	it('replaces relative- with absolute path to codebeamer resources', () => {
 		const mockCbAddress = 'https://mockbeamer.com/cb';
 
-		const expectedResult = `<span class="wikIContent"><p>This is a ref to <img src="${mockCbAddress}/images/issuetypes/user.gif" data-hover-tooltip-target="" class="customizable-tracker-icon" style="background-color:#5f5f5f; margin-right:2px;"><a class="interwikilink" href="${mockCbAddress}/userdata/55" title="[USER:2222]>pjotr</a>, one of our users.</p></span>`;
+		const expectedResult = `<span class="wikIContent"><p>This is a ref to <img src="${mockCbAddress}/images/issuetypes/user.gif" data-hover-tooltip-target="" class="customizable-tracker-icon" style="background-color:#5f5f5f; margin-right:2px;"><a target="_blank" class="interwikilink" href="${mockCbAddress}/userdata/55" title="[USER:2222]>pjotr</a>, one of our users.</p></span>`;
 
 		const result = addContextToCBLinks(mockCbAddress, sampleText);
 
