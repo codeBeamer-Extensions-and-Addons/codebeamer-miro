341f0105a4fc4bc2fff290c800968b4990b715d3
urecha <70019423+urecha@users.noreply.github.com>
2022-12-13 10:11:52 +0100

    refactor itemDetails submit button
    refactor html structure & style
    so that the button will always be at page-bottom, but also
    sticky with a decent margin-bottom
    when space gets small

--- src/pages/item/itemDetails.tsx
+++ src/pages/item/itemDetails.tsx
@@ -612,52 +612,52 @@ export default function ItemDetails(props: { itemId: string; cardId: string }) {
 									data-test={STORY_POINTS_FIELD_NAME}
 								/>
 							</div>
-						</form>
-					</div>
-					{
-						//*********************************************************************** */
-						//********************************SUBMIT********************************* */
-						//*********************************************************************** */
-					}
+							{
+								//*********************************************************************** */
+								//********************************SUBMIT********************************* */
+								//*********************************************************************** */
+							}
 
-					<div className="absolute-bottom">
-						{!animateSuccess && (
-							<button
-								type="submit"
-								disabled={updateItemResult.isFetching}
-								data-test="submit"
-								className={`fade-in button button-primary ${
-									updateItemResult.isFetching
-										? 'button-loading'
-										: ''
-								}`}
-								onClick={() => formik.handleSubmit()}
-							>
-								Save
-							</button>
-						)}
-						{animateSuccess && (
-							<span>
-								<svg
-									className="checkmark"
-									xmlns="http://www.w3.org/2000/svg"
-									viewBox="0 0 52 52"
-								>
-									<circle
-										className="checkmark__circle"
-										cx="26"
-										cy="26"
-										r="25"
-										fill="none"
-									/>
-									<path
-										className="checkmark__check"
-										fill="none"
-										d="M14.1 27.2l7.1 7.2 16.7-16.8"
-									/>
-								</svg>
-							</span>
-						)}
+							<div className="sticky-bottom">
+								{!animateSuccess && (
+									<button
+										type="submit"
+										disabled={updateItemResult.isFetching}
+										data-test="submit"
+										className={`fade-in button button-primary ${
+											updateItemResult.isFetching
+												? 'button-loading'
+												: ''
+										}`}
+										onClick={() => formik.handleSubmit()}
+									>
+										Save
+									</button>
+								)}
+								{animateSuccess && (
+									<span>
+										<svg
+											className="checkmark"
+											xmlns="http://www.w3.org/2000/svg"
+											viewBox="0 0 52 52"
+										>
+											<circle
+												className="checkmark__circle"
+												cx="26"
+												cy="26"
+												r="25"
+												fill="none"
+											/>
+											<path
+												className="checkmark__check"
+												fill="none"
+												d="M14.1 27.2l7.1 7.2 16.7-16.8"
+											/>
+										</svg>
+									</span>
+								)}
+							</div>
+						</form>
 					</div>
 				</div>
 			)}
