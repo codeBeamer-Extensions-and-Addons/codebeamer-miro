405576de17c289c2a1e9f1f291ebe6304174bc7a
Baumgartner <baumgae5@emea.roche.com>
2023-03-07 11:20:43 +0100

    resolves all doesConnectorExist issues

--- src/api/utils/doesConnectorExist.ts
+++ src/api/utils/doesConnectorExist.ts
@@ -1,18 +1,25 @@
 import { Connector } from "@mirohq/websdk-types";
+import { Relation } from "../../models/relation.if";
 
-export default async function doesConnectorExist(startCardId: string, endCardId: string) {
-  const connectors = await miro.board.get({ type: 'connector' }) as Connector[];
+export default async function doesConnectorExist(
+  startCardId: string,
+  endCardId: string
+) {
+  const connectors = (await miro.board.get({
+    type: "connector",
+  })) as Connector[];
 
   let connectorExists = false;
 
   for (const connector of connectors) {
-    const itemData = await connector.getMetadata('relation');
-    const start = itemData['startCardId'];
-    const end = itemData['endCardId'];
+    const itemData = (await connector.getMetadata("relation")) as Relation;
 
-    if (start === startCardId && end === endCardId) {
+    if (
+      itemData.startCardId === startCardId &&
+      itemData.endCardId === endCardId
+    ) {
       connectorExists = true;
-      break;
+      return connectorExists;
     }
   }
 
