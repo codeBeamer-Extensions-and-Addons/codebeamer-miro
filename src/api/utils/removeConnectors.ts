d85b335f51746dd76a82d3e1db86d9465bed56d7
Baumgartner <baumgae5@emea.roche.com>
2023-03-15 09:50:32 +0100

    fixed removeConnectors issue

--- src/api/utils/removeConnectors.ts
+++ src/api/utils/removeConnectors.ts
@@ -11,45 +11,13 @@ export default async function removeConnectros(
     type: "connector",
   })) as Connector[];
 
-  for (const association of associations) {
-    const endCardIds = await getAppCardIds(association.targetItemId);
+  const connectorsToStartCard = connectors.filter((connector) => {
+    return connector.start.item === startCardId;
+  });
 
-    for (const endCardId of endCardIds) {
-      for (const connector of connectors) {
-        const itemData = (await connector.getMetadata("relation")) as Relation;
+  connectorsToStartCard.forEach(async (connector) => {
+    await miro.board.remove(connector);
+  });
 
-        if (
-          itemData.startCardId === startCardId &&
-          itemData.endCardId === endCardId
-        ) {
-          try {
-            await miro.board.remove(connector);
-          } catch (error) {
-            console.log("error: ", error);
-          }
-        }
-      }
-    }
-  }
-
-  for (const downstreamRef of downstreamRefs) {
-    const endCardIds = await getAppCardIds(downstreamRef);
-
-    for (const endCardId of endCardIds) {
-      for (const connector of connectors) {
-        const itemData = (await connector.getMetadata("relation")) as Relation;
-
-        if (
-          itemData.startCardId === startCardId &&
-          itemData.endCardId === endCardId
-        ) {
-          try {
-            await miro.board.remove(connector);
-          } catch (error) {
-            console.log("error: ", error);
-          }
-        }
-      }
-    }
-  }
+  console.log(connectorsToStartCard);
 }
