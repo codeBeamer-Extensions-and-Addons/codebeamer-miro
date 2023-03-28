6b1af4396248140aaad787e6d5bcd320d8137d44
urecha <70019423+urecha@users.noreply.github.com>
2023-03-28 11:00:37 +0200

    add ZoomToItemButton in ItemActions

--- src/pages/item/item-actions/ItemActions.tsx
+++ src/pages/item/item-actions/ItemActions.tsx
@@ -1,15 +1,17 @@
-import React from "react";
-import LoadDownstreamReferencesButton from "./itemActionsButtons/loadDownstreamReferencesButton/loadDownstreamReferencesButton";
-import LoadRelationsButton from "./itemActionsButtons/loadRelationsButton/loadRelationsButton";
+import React from 'react';
+import LoadDownstreamReferencesButton from './itemActionsButtons/loadDownstreamReferencesButton/loadDownstreamReferencesButton';
+import LoadRelationsButton from './itemActionsButtons/loadRelationsButton/loadRelationsButton';
+import ZoomToItemButton from './itemActionsButtons/zoomToItemButton/ZoomToItemButton';
 
 export default function ItemActions(props: {
-  itemId: string | number;
-  cardId: string | number;
+	itemId: string | number;
+	cardId: string | number;
 }) {
-  return (
-    <div>
-      <LoadDownstreamReferencesButton itemId={props.itemId} />
-      <LoadRelationsButton itemId={props.itemId} />
-    </div>
-  );
+	return (
+		<div>
+			<LoadDownstreamReferencesButton itemId={props.itemId} />
+			<LoadRelationsButton itemId={props.itemId} />
+			<ZoomToItemButton cardId={props.cardId} />
+		</div>
+	);
 }
