3903ecfaaf1c245d5e5d57301bbfd801de1424b7
urecha <70019423+urecha@users.noreply.github.com>
2023-09-28 12:26:32 +0200

    update LucidExport.cy.tsx
    add test for actually downloading the json

--- src/pages/import/components/settings/lucidExport/LucidExport.cy.tsx
+++ src/pages/import/components/settings/lucidExport/LucidExport.cy.tsx
@@ -36,6 +36,44 @@ describe('<LucidExport', () => {
                 expect(stubBoardGet).to.have.been.called;
             })
         });
+
+        it('downloads a json file on click', () => {
+            const stubSync = cy.stub();
+
+            const itemOne: Partial<AppCard> = {
+                id: '1',
+                x: 1,
+                y: 2,
+                title: '[RETUS-1]',
+                sync: stubSync,
+            };
+            const itemTwo: Partial<AppCard> = {
+				id: '2',
+				x: 2,
+				y: 1,
+				title: '[RETUS-2]',
+				sync: stubSync,
+			};
+            cy.stub(miro.board, 'get').callsFake(() => {
+				return Promise.resolve([itemOne, itemTwo]);
+			});
+
+            cy.mountWithStore(<LucidExport />);
+            cy.get('button').contains('Export').click().then(() => {
+                cy.wait(100);
+                //file should be downloaded into cypress/downloads
+                cy.readFile(
+					`cypress/downloads/miro-export-${new Date()
+						.toISOString()
+						.substring(0, 10)}.json`
+				).then((file) => {
+					expect(file).to.deep.equal([
+                        { id: itemOne.id, coordinates: { x: itemOne.x, y: itemOne.y } },
+                        { id: itemTwo.id, coordinates: { x: itemTwo.x, y: itemTwo.y } },
+                    ]);
+				});
+            });
+        });
     })
 });
 
